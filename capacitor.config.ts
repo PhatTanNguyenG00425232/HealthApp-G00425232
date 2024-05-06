4a7401f4d67733ed73c424852977422785d3ed19
unknown <g00425232@atu.ie>
2024-05-06 11:31:30 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'healthApp',
+  webDir: 'www'
+};
+
+export default config;
